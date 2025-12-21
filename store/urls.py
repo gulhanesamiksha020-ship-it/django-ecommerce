fd4ec3b713d13ab5779d48387236af3c82bbe1ff
Samiksha Gulhane <gulhanesamksha020@gmail.com>
2025-12-21 22:15:39 +0530

    Added category slug-based URLs and product filtering

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('category/<int:cat_id>/', views.category_products, name='category_products'),
+    path('product/<int:id>/', views.product_details, name='product_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from .models import Product , Category
+
 
 def home(request):
-    return HttpResponse("Store Home Page Working")
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    return render(request, 'store/home.html', {
+        'products': products,
+        'categories': categories
+    })
+
+# vew displayed products by category
+def category_products(request,cat_id):
+    categories = Category.objects.all()
+    products = Product.objects.filter(category_id=cat_id)
+    return render(request, 'store/home.html', {
+        'products': products,
+        'categories': categories
+    })
+
+# view for specific product details
+
+from django.shortcuts import get_object_or_404
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    return render(request, 'store/product_detail.html', {'product': product})
+
+
